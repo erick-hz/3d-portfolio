0f5d2dc2865dab3e1b89d9e38d6ca27543ae70dd
Erick Hernandez <yerickk8@gmail.com>
2024-01-11 17:29:12 -0600

    Enviroment set-up

---
+++ src/constants/index.js
@@ -0,0 +1,211 @@
+import { meta, mullenLowe, kaiber, pixelbot } from "../assets/images"
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+  {
+    title: "Front-end Engineer",
+    company_name: "Kaiber",
+    icon: kaiber,
+    iconBg: "#000",
+    date: "July 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Front-end Engineer",
+    company_name: "Pixelbot",
+    icon: pixelbot,
+    iconBg: "#fbc3bc",
+    date: "July 2021 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Front-end Engineer",
+    company_name: "Mullenlowe",
+    icon: mullenLowe,
+    iconBg: "#b7e4c7",
+    date: "January 2021 - July 2021",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+]
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/erick-hz",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/erick-hernandez-93361618b/",
+  },
+]
+
+export const projects = [
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "Kaiber",
+    description:
+      "Bring your dreams to life Tell stories like never before with our advanced AI generation engine.",
+    link: "https://kaiber.ai/",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Personal porfolio",
+    description: "I build things for the web.",
+    link: "https://erickdev.netlify.app/",
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "lavish",
+    description:
+      "Get access to our exclusive diamond card that allows you to send unlimited transactions without getting charged any fees",
+    link: "https://lavish-bx.web.app/",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "Marble Slab Canada",
+    description:
+      "Get your favourite treats with the NEW MARBLE SLAB mobile ordering app.",
+    link: "https://apps.apple.com/us/app/marble-slab-canada/id1599580387",
+  },
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "Kaiber App",
+    description: "AI Art Lab: Artist-Built",
+    link: "https://apps.apple.com/us/app/kaiber/id6458980808",
+  }
+]

3f051bdff0f1c05c5dea09db6f8ce0bdccd36156
Erick Hernandez <yerickk8@gmail.com>
2025-09-28 12:26:08 -0600

    feat: add ASCII cat signature in console

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+console.log(
+  "%c /\\_/\\  \n( o.o ) \n > ^ <   ~ Yerick",
+  "color: purple; font-size: 14px; font-weight: bold;"
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
